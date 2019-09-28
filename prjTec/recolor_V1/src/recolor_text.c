bc8e9624ce4421ea875c5ea48dc8feded51df65b
charles66820 <charles.goedefroit@gmail.com>
2019-09-29 00:22:47 +0200

    prj test

--- prjTec/recolor_V1/src/recolor_text.c
+++ prjTec/recolor_V1/src/recolor_text.c
@@ -4,19 +4,37 @@
 
 
 int main(void) {
-        bool win = false;
-        game g = game_new_empty();
+    bool over = false;
+    /*
+    game g = game_new_empty();
+    game_set_cell_init(g, SIZE, SIZE, RED);
+    game_set_max_moves(g, SIZE);//*/
 
-        while (!win)
+    //*
+    int test[2] = {SIZE, SIZE};
+    game g = game_new(test, SIZE);//*/
+
+    char choice;
+    int input;
+
+    while (!over)
+    {
+        printf("choisissez une couleur en écrivent un des chiffre suivant :\n 0 = red\n 1 = green\n 2 = blue\n 3 = yellow\n Pour relancer une partie écrivez \"r\"\n Pour quiter écrivez \"q\"\n");
+        input = getchar();
+        if (input != EOF)
         {
-                game_
+            choice = input;
         }
 
+        if (choice == *"r")
+            game_restart(g);
+        else if (choice == *"q")
+            game_delete(g);
+        else if (choice >= 0 && choice < NB_COLORS)
+            game_play_one_move(g, (color) choice);
+        over = game_is_over(g);
+    }
 
-
-
-
-        //game g = game_new(RED, 12);
-        game_delete(g);
-        return EXIT_SUCCESS;
+    game_delete(g);
+    return EXIT_SUCCESS;
 }
