77b6162ed233a33dedd29823ccc42f70e18d7838
Charles <charles.goedefroit@etu.u-bordeaux.fr>
2019-09-28 11:59:32 +0200

    projet techno

--- prjTec/recolor_V1/src/recolor_text.c
+++ prjTec/recolor_V1/src/recolor_text.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 #include "game.h"
 
+
 int main(void) {
-    game g = game_new_empty();//game g = game_new(RED, 12);
-    game_delete(g);
-    return EXIT_SUCCESS;
+        bool win = false;
+        game g = game_new_empty();
+
+        while (!win)
+        {
+                game_
+        }
+
+
+
+
+
+        //game g = game_new(RED, 12);
+        game_delete(g);
+        return EXIT_SUCCESS;
 }
