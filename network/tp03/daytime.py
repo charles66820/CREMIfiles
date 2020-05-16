ed3970eb60a7eaa15bf64fdedc30da327d09325e
charles66820 <charles.goedefroit@gmail.com>
2020-05-16 17:16:43 +0200

    network end

--- network/tp03/daytime.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.6
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "time-c.nist.gov"
-port = 13
-s.connect((host, port))
-data = s.recv(1024)
-print(data)
-s.close()
--- network/tp05/src/js/home.js
+++
@@ -1,9 +0,0 @@
-let show = true;
-function toggleList() {
-    let elem = document.getElementById("listAMasque");
-    if (show)
-        elem.style.visibility = "hidden";
-    else
-        elem.style.visibility = "visible";
-    show = !show;
-}
