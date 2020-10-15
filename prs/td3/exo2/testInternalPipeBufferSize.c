6e5495d7cc8e54605922194552a3ef09966c5c95
charles66820 <charles.goedefroit@gmail.com>
2020-10-15 21:36:18 +0200

    exo 2 et 3 fini

---
+++ prs/td3/exo2/testInternalPipeBufferSize.c
@@ -0,0 +1,23 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  // pipe
+  int pipe1[2];
+  pipe(pipe1);
+
+  int n;
+  double t = 0;
+
+  do {
+    n = write(pipe1[1], "k", sizeof(char)); // block here after 65536 char
+    t++;
+    printf("%f time\n", t);
+  } while(n);
+
+  close(pipe1[1]);
+  return EXIT_SUCCESS;
+}
