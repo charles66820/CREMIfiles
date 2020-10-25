7dfe81c16d5bef936f77d959cf7ca2055c76dcc2
charles66820 <charles.goedefroit@gmail.com>
2020-10-25 22:30:23 +0100

    backgrand command

--- prs/project/src/Evaluation.c
+++ prs/project/src/Evaluation.c
@@ -7,52 +7,104 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+//#include <signal.h>
+
 #include "Shell.h"
 
 typedef unsigned int uint;
+#define BUFSIZE 1000
 
-int evaluer_expr(Expression *e) {
+static char** getArgs(int pid) {
+  char cmdLineFile[24];
+  sprintf(cmdLineFile, "/proc/%d/cmdline", pid);
+  FILE* f = fopen(cmdLineFile, "r");
+  if (f == NULL) {
+    fprintf(stdout, "Cannot create %s file\n", cmdLineFile);
+    exit(EXIT_FAILURE);
+  }
+
+  char** s = InitialiserListeArguments();
+  while (!feof(f)) {
+    char buff[BUFSIZE];
+    if (fgets(buff, BUFSIZE - 1, f) != NULL) s = AjouterArg(s, buff);
+  }
+  fclose(f);
+  return s;
+}
+
+int evaluer_expr(Expression* e) {
+  int status;
+  // Process exptression
   switch (e->type) {
     case VIDE:
-      return 0;
+      status = 0;
+      break;
     case SIMPLE: {
       int argc = LongueurListe(e->arguments);
       if (!strcmp(e->arguments[0], "echo")) {
         for (uint i = 1; i < argc; i++) {
           printf("%s", e->arguments[i]);
-          if (i == argc - 1) printf("\n");
-          else printf(" ");
+          if (i == argc - 1)
+            printf("\n");
+          else
+            printf(" ");
         }
+        status = 0;
       } else {
-        if (!fork()) {
+        int pid;
+        if (!(pid = fork())) {
           execvp(e->arguments[0], e->arguments);
 
           fprintf(stderr, "%s: command not found\n", e->arguments[0]);
-          return 127;
+          status = 127;
         }
-        int exitCode;
-        wait(&exitCode);
-        return WTERMSIG(exitCode)? WTERMSIG(exitCode) + 128 : WEXITSTATUS(exitCode);
+        int exitStatus;
+        waitpid(pid, &exitStatus, 0);
+        status = WTERMSIG(exitStatus) ? WTERMSIG(exitStatus) + 128
+                                      : WEXITSTATUS(exitStatus);
       }
-      return 0;
+      break;
     }
     case SEQUENCE: {
       evaluer_expr(e->gauche);
-      return evaluer_expr(e->droite);
+      status = evaluer_expr(e->droite);
+      break;
     }
     case SEQUENCE_ET: {
-      int status = evaluer_expr(e->gauche);
-      if (!status) return evaluer_expr(e->droite);
-      return status;
+      status = evaluer_expr(e->gauche);
+      if (!status) status = evaluer_expr(e->droite);
+      break;
     }
     case SEQUENCE_OU: {
-      int status = evaluer_expr(e->gauche);
-      if (!status) return status;
-      return evaluer_expr(e->droite);
+      status = evaluer_expr(e->gauche);
+      if (status) status = evaluer_expr(e->droite);
+      break;
+    }
+    case BG: {
+      if (!fork()) exit(evaluer_expr(e->gauche));
+      status = 0;
+      break;
     }
 
     default:
-      fprintf(stderr, "not yet implemented \n");
-      return 1;
+      fprintf(stderr, "not yet implemented\n");
+      status = 1;
   }
+
+  // Process backgrand process
+  pid_t pid;
+  int exitStatus;
+  int count = 1;
+  if (pid = waitpid(-1, &exitStatus, WNOHANG) > 0) {
+    char* s = !WEXITSTATUS(exitStatus)
+                  ? "Done"
+                  : WEXITSTATUS(exitStatus) == 1
+                        ? "Exit 1"
+                        : WEXITSTATUS(exitStatus) > 128
+                              ? strsignal(WEXITSTATUS(exitStatus) - 128)
+                              : "Unkown exit code";
+    printf("[%d]\t%s\n", count, s);
+    count++;
+  }
+  return status;
 }
