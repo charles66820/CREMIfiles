17d9f1f2e56418fac7b48b521535b99b9e846666
charles66820 <charles.goedefroit@gmail.com>
2020-11-20 03:25:40 +0100

    improuve SIGCHLD handle and try to fix bug

--- prs/project/src/Evaluation.c
+++ prs/project/src/Evaluation.c
@@ -15,26 +15,21 @@ typedef unsigned int uint;
 
 void childSigHandler(int sig) {
   int exitStatus;
-  int oldpid = wait(&exitStatus);
-  if (oldpid != -1) {
+  int oldpid = waitpid(0, &exitStatus, WNOHANG); // BUG: call by children
+  if (oldpid != -1 && oldpid != 0) {
     if (WIFSIGNALED(exitStatus))
-      printf("\n\e[ABackground process %d finish with : %s\n", oldpid, strsignal(exitStatus));
+      printf("\n\e[ABackground process %d finish with : %s\n", oldpid,
+             strsignal(exitStatus));
     else if (WEXITSTATUS(exitStatus) == 0)
       printf("\n\e[ABackground process %d finish with : %s\n", oldpid, "Done");
     else
-      printf("\n\e[ABackground process %d finish with : Exit %d\n", oldpid, WEXITSTATUS(exitStatus));
+      printf("\n\e[ABackground process %d finish with : Exit %d\n", oldpid,
+             WEXITSTATUS(exitStatus));
   }
 }
 
-struct sigaction sa;
-int evaluer_expr(Expression* e) {
-  // Process backgrand process
-  sa.sa_flags = 0;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_handler = childSigHandler;
-  sigaction(SIGCHLD, &sa, NULL);
-  sigprocmask(SIG_BLOCK, &sa.sa_mask, NULL); // block SIGCHLD
-
+struct sigaction sa, oldSa;
+int evaluer_exprL(Expression* e) {
   int status;
   // Process exptression
   switch (e->type) {
@@ -45,7 +40,7 @@ int evaluer_expr(Expression* e) {
       if (!strcmp(e->arguments[0], "echo")) {
         for (uint i = 1; e->arguments[i] != NULL; i++)
           printf("%s ", e->arguments[i]);
-        printf("\n");
+        printf("\e[D\n");
         status = 0;
       } else if (!strcmp(e->arguments[0], "exit")) {
         exit(EXIT_SUCCESS);
@@ -123,23 +118,24 @@ int evaluer_expr(Expression* e) {
       break;
     }
     case SEQUENCE: {
-      evaluer_expr(e->gauche);
-      status = evaluer_expr(e->droite);
+      evaluer_exprL(e->gauche);
+      status = evaluer_exprL(e->droite);
       break;
     }
     case SEQUENCE_ET: {
-      status = evaluer_expr(e->gauche);
-      if (!status) status = evaluer_expr(e->droite);
+      status = evaluer_exprL(e->gauche);
+      if (!status) status = evaluer_exprL(e->droite);
       break;
     }
     case SEQUENCE_OU: {
-      status = evaluer_expr(e->gauche);
-      if (status) status = evaluer_expr(e->droite);
+      status = evaluer_exprL(e->gauche);
+      if (status) status = evaluer_exprL(e->droite);
       break;
     }
     case BG: {
-      if (!fork()) {
-        int retStatus = evaluer_expr(e->gauche);
+      if (!fork()) { // FIXME: idk
+        sigaction(SIGCHLD, &oldSa, NULL);  // Disable SIGINT handler REVIEW: remove ?
+        int retStatus = evaluer_exprL(e->gauche);
         if (retStatus > 128)
           raise(retStatus - 128);
         else
@@ -157,11 +153,11 @@ int evaluer_expr(Expression* e) {
         break;
       }
 
-      int oldFD = dup(STDOUT_FILENO);    // Save stdout
-      dup2(fd, STDOUT_FILENO);           // change stdout with file descriptor
-      close(fd);                         // close file descriptor
-      status = evaluer_expr(e->gauche);  // Exec left tree
-      dup2(oldFD, STDOUT_FILENO);        // Reset stdout
+      int oldFD = dup(STDOUT_FILENO);     // Save stdout
+      dup2(fd, STDOUT_FILENO);            // change stdout with file descriptor
+      close(fd);                          // close file descriptor
+      status = evaluer_exprL(e->gauche);  // Exec left tree
+      dup2(oldFD, STDOUT_FILENO);         // Reset stdout
       break;
     }
     case REDIRECTION_I: {
@@ -172,11 +168,11 @@ int evaluer_expr(Expression* e) {
         break;
       }
 
-      int oldFD = dup(STDIN_FILENO);     // Save stdin
-      dup2(fd, STDIN_FILENO);            // change stdin with file descriptor
-      close(fd);                         // close file descriptor
-      status = evaluer_expr(e->gauche);  // Exec left tree
-      dup2(oldFD, STDIN_FILENO);         // Reset stdin
+      int oldFD = dup(STDIN_FILENO);      // Save stdin
+      dup2(fd, STDIN_FILENO);             // change stdin with file descriptor
+      close(fd);                          // close file descriptor
+      status = evaluer_exprL(e->gauche);  // Exec left tree
+      dup2(oldFD, STDIN_FILENO);          // Reset stdin
       break;
     }
     case REDIRECTION_A: {
@@ -189,11 +185,11 @@ int evaluer_expr(Expression* e) {
 
       lseek(fd, 0, SEEK_END);  // Go to end of file
 
-      int oldFD = dup(STDOUT_FILENO);    // Save stdout
-      dup2(fd, STDOUT_FILENO);           // change stdout with file descriptor
-      close(fd);                         // close file descriptor
-      status = evaluer_expr(e->gauche);  // Exec left tree
-      dup2(oldFD, STDOUT_FILENO);        // Reset stdout
+      int oldFD = dup(STDOUT_FILENO);     // Save stdout
+      dup2(fd, STDOUT_FILENO);            // change stdout with file descriptor
+      close(fd);                          // close file descriptor
+      status = evaluer_exprL(e->gauche);  // Exec left tree
+      dup2(oldFD, STDOUT_FILENO);         // Reset stdout
       break;
     }
     case REDIRECTION_E: {
@@ -205,11 +201,11 @@ int evaluer_expr(Expression* e) {
         break;
       }
 
-      int oldFD = dup(STDERR_FILENO);    // Save stderr
-      dup2(fd, STDERR_FILENO);           // change stderr with file descriptor
-      close(fd);                         // close file descriptor
-      status = evaluer_expr(e->gauche);  // Exec left tree
-      dup2(oldFD, STDERR_FILENO);        // Reset stderr
+      int oldFD = dup(STDERR_FILENO);     // Save stderr
+      dup2(fd, STDERR_FILENO);            // change stderr with file descriptor
+      close(fd);                          // close file descriptor
+      status = evaluer_exprL(e->gauche);  // Exec left tree
+      dup2(oldFD, STDERR_FILENO);         // Reset stderr
       break;
     }
     case REDIRECTION_EO: {
@@ -223,12 +219,12 @@ int evaluer_expr(Expression* e) {
 
       int oldStdoutFD = dup(STDOUT_FILENO);  // Save stdout
       int oldStderrFD = dup(STDERR_FILENO);  // Save stderr
-      dup2(fd, STDOUT_FILENO);           // change stdout with file descriptor
-      dup2(fd, STDERR_FILENO);           // change stderr with file descriptor
-      close(fd);                         // close file descriptor
-      status = evaluer_expr(e->gauche);  // Exec left tree
-      dup2(oldStdoutFD, STDOUT_FILENO);  // Reset stdout
-      dup2(oldStderrFD, STDERR_FILENO);  // Reset stderr
+      dup2(fd, STDOUT_FILENO);            // change stdout with file descriptor
+      dup2(fd, STDERR_FILENO);            // change stderr with file descriptor
+      close(fd);                          // close file descriptor
+      status = evaluer_exprL(e->gauche);  // Exec left tree
+      dup2(oldStdoutFD, STDOUT_FILENO);   // Reset stdout
+      dup2(oldStderrFD, STDERR_FILENO);   // Reset stderr
       break;
     }
     case PIPE: {
@@ -246,7 +242,7 @@ int evaluer_expr(Expression* e) {
         // redirect left cmd stdout to pipe write
         dup2(pipe1[1], STDOUT_FILENO);
         // left cmd
-        exit(evaluer_expr(e->gauche));
+        exit(evaluer_exprL(e->gauche));
       } else {
         int pidRight = fork();
         if (!pidRight) {
@@ -255,7 +251,7 @@ int evaluer_expr(Expression* e) {
           // redirect pipe read to right cmd stdin
           dup2(pipe1[0], STDIN_FILENO);
           // right cmd
-          exit(evaluer_expr(e->droite));
+          exit(evaluer_exprL(e->droite));
         } else {
           // wait left cmd
           waitpid(pidLeft, NULL, 0);
@@ -277,8 +273,21 @@ int evaluer_expr(Expression* e) {
       status = 1;
   }
 
-  sigprocmask(SIG_UNBLOCK, &sa.sa_mask, NULL); // unblock SIGCHLD
-  sigprocmask(SIG_BLOCK, &sa.sa_mask, NULL); // reblock SIGCHLD
-
   return status;
+}
+
+int evaluer_expr(Expression* e) {
+  // Process backgrand process
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = childSigHandler;
+  sigaction(SIGCHLD, &sa, &oldSa);
+
+  sigaddset(&sa.sa_mask, SIGCHLD);            // add to mask
+  sigprocmask(SIG_BLOCK, &sa.sa_mask, NULL);  // block SIGCHLD
+
+  evaluer_exprL(e);
+
+  sigprocmask(SIG_UNBLOCK, &sa.sa_mask, NULL);  // unblock SIGCHLD
+  sigprocmask(SIG_BLOCK, &sa.sa_mask, NULL);    // reblock SIGCHLD
 }
