4f12e923a4658c57a5c24796eb96e8bda599cdb9
charles66820 <charles.goedefroit@gmail.com>
2021-02-04 17:38:25 +0100

    oui

--- projet/tm/src/main/java/imageProcessing/Convolution.java
+++ projet/tm/src/main/java/imageProcessing/Convolution.java
@@ -242,7 +242,6 @@ public class Convolution {
         endTime = System.nanoTime();
         System.out.println("my gauss convolution (in " + (endTime - starTime) + "ns)");
         saveImage(output, "myGaussConvolutionWithKernel", outPath);//*/
-        DebugInfo.showDebugInfo(input, output, null, null);
 
         //*
         starTime = System.nanoTime();
@@ -251,6 +250,6 @@ public class Convolution {
         System.out.println("default gauss convolution (in " + (endTime - starTime) + "ns)");
         saveImage(output, "defaultGaussConvolutionWithKernel", outPath);//*/
 
-       // DebugInfo.showDebugInfo(input, output, null, null);
+       DebugInfo.showDebugInfo(input, output, null, null);
     }
 }
