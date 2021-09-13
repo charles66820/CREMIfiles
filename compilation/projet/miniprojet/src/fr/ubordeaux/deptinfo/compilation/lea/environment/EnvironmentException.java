6584f76f69ba1fb1b5f2879abb4cb2357e89ad15
charles66820 <charles.goedefroit@gmail.com>
2021-09-13 11:51:18 +0200

    remove compilation

--- compilation/projet/miniprojet/src/fr/ubordeaux/deptinfo/compilation/lea/environment/EnvironmentException.java
+++
@@ -1,9 +0,0 @@
-package fr.ubordeaux.deptinfo.compilation.lea.environment;
-
-public class EnvironmentException extends Exception {
-
-	public EnvironmentException(String msg) {
-		super("*** Environment error: " + msg);
-	}
-
-}
