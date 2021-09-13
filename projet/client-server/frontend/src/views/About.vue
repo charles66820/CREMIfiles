9c7f7975cccc79f627dba477b364e5b8dbb2736d
charles66820 <charles.goedefroit@gmail.com>
2021-09-13 12:08:14 +0200

    remove pdl

--- projet/client-server/frontend/src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <article>
-    <h1>About</h1>
-    <section>
-      <h2></h2>
-      <p>This is an about page</p>
-    </section>
-  </article>
-</template>
--- projet/image/README.md
+++
@@ -1,5 +0,0 @@
-#
-
-## Cmd
-
-`mvn exec:java -Dexec.mainClass=imageProcessing.GrayLevelProcessing "-Dexec.args=images/paysage3552x2368.jpg images/output/"`
