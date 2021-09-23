ac2721e16b26c8ca02e470c59bc94ebaca2b978d
charles66820 <charles.goedefroit@gmail.com>
2021-09-24 00:34:52 +0200

    add todo

--- os/nachos/code/threads/thread.cc
+++ os/nachos/code/threads/thread.cc
@@ -242,7 +242,7 @@ void Thread::Yield() {
   ASSERT(this == currentThread);
 
   DEBUG('t', "Yielding thread %p \"%s\"\n", this, getName());
-
+  // TODO: 5.4 1/2 td01
   nextThread = scheduler->FindNextToRun();
   if (nextThread != NULL) {
     scheduler->ReadyToRun(this);
