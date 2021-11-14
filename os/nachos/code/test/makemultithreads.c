764bd53bd398a49ba650a692cbb44e5831349bf9
charles66820 <charles.goedefroit@gmail.com>
2021-11-14 18:56:52 +0100

    add test Co-authored-by: Asheila <Asheila@users.noreply.github.com>

---
+++ os/nachos/code/test/makemultithreads.c
@@ -0,0 +1,32 @@
+#ifdef CHANGED
+#include "syscall.h"
+
+void f (void *arg) {
+    volatile char c = (char)(int)arg;
+    PutString("Thread : ");
+    PutChar(c);
+    PutChar('!\n');
+    ThreadExit(); // TEST: error without this syscall
+}
+
+int main () {
+    char c = 'a';
+    void *arg = (void *)(int)c;
+    PutString("Main thread !\n");
+
+    // Creation of multiple threads
+    int i;
+    for (i = 0; i < 26; i++) {
+        int newThread = ThreadCreate(*f, arg);
+        c = c + 1;
+        arg = (void *)(int)c;
+    }
+
+    PutString("Main thread finish\n");
+    ThreadExit(); // TEST: with and without this syscall
+
+    return 0;
+}
+
+
+#endif // CHANGED
