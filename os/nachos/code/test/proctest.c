aa0511428f6a352fcc1439b941e259fae999555a
charles66820 <charles.goedefroit@gmail.com>
2021-12-15 17:45:40 +0100

    fix for cremi

--- os/nachos/code/test/proctest.c
+++ os/nachos/code/test/proctest.c
@@ -4,10 +4,10 @@
 const int N = 12;
 
 int main() {
+  int i;
   PutString("Father proc!\n");
 
   ForkExec("test/userpages0");
-  int i;
   for (i = 0; i < N; i++) ForkExec("test/userpages1");
 
   return 0;
