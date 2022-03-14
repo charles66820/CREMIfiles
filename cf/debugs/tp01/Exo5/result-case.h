9f63eef95a7fde69d880d22a49ec70fb26cd5acb
charles66820 <charles.goedefroit@gmail.com>
2022-03-14 17:07:08 +0100

    continue

--- cf/debugs/tp01/Exo5/result-case.h
+++ cf/debugs/tp01/Exo5/result-case.h
@@ -1,8 +1,12 @@
 /*@
+  predicate smallerThen(integer a, integer b, integer c) = a < b < c || a < c < b; // a < b&&c
+*/
+
+/*@
   ensures \result >= 0 || \result <= 3;
   ensures a==b || b==c || a==c ==> \result == 0; // \result == 0 when a==b || b==c || a==c
-  ensures a < b < c || a < c < b ==> \result == 1; // \result == 1 when a < b&&c
-  ensures b < a < c || b < c < a ==> \result == 2; // \result == 2 when b < a&&c
-  ensures c < a < b || c < b < a ==> \result == 3; // \result == 3 when c < a&&b
+  ensures smallerThen(a, b, c) ==> \result == 1; // \result == 1 when a < b&&c
+  ensures smallerThen(b, a, c) ==> \result == 2; // \result == 2 when b < a&&c
+  ensures smallerThen(c, a, b) ==> \result == 3; // \result == 3 when c < a&&b
 */
 int caseResult(int a, int b, int c);
