940a7585f7609292f267a09d0da5fc3ac7d33f9d
charles66820 <charles.goedefroit@gmail.com>
2022-03-17 13:15:30 +0100

    exo 2

--- cf/debugs/tp02/Exo2/exo2.h
+++ cf/debugs/tp02/Exo2/exo2.h
@@ -1,7 +1,20 @@
 #include "limits.h"
 
-/* Mettez votre spécification ici.*/
+/*@
+  requires \valid(a);
+  requires \valid(b);
+  ensures *a <= *b;
+  ensures (\old(*a) == *a && \old(*b) == *b) || (\old(*a) == *b && \old(*b) == *a);
+*/
 void sort_ptr(int* a, int* b);
 
-/* Mettez votre spécification ici. Attention, on demande de plus que la valeur de *b ne soit pas modifiée.*/
+/*@
+  requires \valid(a);
+  requires \valid_read(b);
+  requires \separated(a, b);
+  requires *a + *b >= INT_MIN;
+  requires *a + *b <= INT_MAX;
+  ensures *a == \old(*a) + \old(*b);
+  ensures \old(*b) == *b;
+*/
 void sum_in_pointer(int* a, int* b);
