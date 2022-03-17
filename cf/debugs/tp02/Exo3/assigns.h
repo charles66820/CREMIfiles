be610693ffe1c57732ef766bc234096051a478ea
charles66820 <charles.goedefroit@gmail.com>
2022-03-17 13:34:56 +0100

    Exo3

--- cf/debugs/tp02/Exo3/assigns.h
+++ cf/debugs/tp02/Exo3/assigns.h
@@ -1,17 +1,24 @@
 #include "limits.h"
 
-/*@ requires \valid(a);
-  @ requires *a+1 <= INT_MAX;
-  @ ensures *a == \old(*a+1);
+/*@
+  requires \valid(a);
+  requires *a + 1 <= INT_MAX;
+  assigns *a;
+  ensures *a == \old(*a + 1);
 */
 void add_one(int *a);
 
+/*@
+  assigns \nothing;
+*/
 void dummy();
 
-/*@ requires \valid(a) && \valid(b);
-  @ requires *a+1 <= INT_MAX;
-  @ ensures \result == 1;
-  @ ensures *a == \old(*a+1);
-  @ ensures *b == \old(*b);
+/*@
+  requires \valid(a) && \valid(b);
+  requires *a+1 <= INT_MAX;
+  requires \separated(a, b);
+  ensures \result == 1;
+  ensures *a == \old(*a+1);
+  ensures *b == \old(*b);
 */
 int f(int *a, int *b);
