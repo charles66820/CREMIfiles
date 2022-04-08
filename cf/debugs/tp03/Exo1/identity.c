fdfae314ab49b76ceda02c30d95a209d23e532ef
charles66820 <charles.goedefroit@gmail.com>
2022-04-08 11:25:27 +0200

    continue tp03

--- cf/debugs/tp03/Exo1/identity.c
+++ cf/debugs/tp03/Exo1/identity.c
@@ -2,6 +2,11 @@
 
 int identity_while(int n){
 	int result = 0;
+
+	/*@
+  	loop invariant 0 <= result <= n;
+		loop assigns result;
+	*/
 	while (result < n){
 		result++;
 	}
@@ -10,6 +15,13 @@ int identity_while(int n){
 
 int identity_for(int n){
 	int result = 0;
+
+	/*@
+  	loop invariant result == i - 1;
+  	loop invariant 0 <= result <= n;
+  	loop invariant 1 <= i <= n + 1;
+		loop assigns result, i;
+	*/
 	for (int i = 1; i <=n; i++){
 		result++;
 	}
--- cf/debugs/tp03/Exo2/behavioral_loop.c
+++ cf/debugs/tp03/Exo2/behavioral_loop.c
@@ -2,6 +2,18 @@
 
 int behavioral_loop(int n, int c){
 	int result = 0;
+
+  /*@
+    loop invariant 1 <= i <= n + 1;
+    loop assigns result, i;
+    loop invariant 0 <= result <= i * 2 - 2; // fix rte
+    for cPos:
+      loop invariant result == 2 * i - 2;
+      loop invariant 0 <= result <= n * 2;
+    for cNeg:
+      loop invariant result == i - 1;
+      loop invariant 0 <= result <= n;
+  */
 	for(int i = 1; i <= n; i++){
 		if(c >= 0) result++;
 		result++;
