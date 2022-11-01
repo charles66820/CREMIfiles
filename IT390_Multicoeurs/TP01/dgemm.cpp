93fef03e39ea739ce57a925c8fe4eb0e5f57b864
charles66820 <charles.goedefroit@gmail.com>
2022-11-01 12:27:01 +0100

    continue but bugged

--- IT390_Multicoeurs/TP01/dgemm.cpp
+++ IT390_Multicoeurs/TP01/dgemm.cpp
@@ -19,12 +19,32 @@ void dgemm(type* A, type* B, type* C) {
       for (k = 0; k < N; k++) C[i * N + j] += A[i * N + k] * B[k * N + j];
 }
 
+static mipp::Reg<type> v1;
+static mipp::Reg<type> v2;
+
+/**
+ * @brief dgemm
+ *
+ * @param A fist matrix
+ * @param B second matrix /!\ need to be transposed
+ * @param C Result matrix
+ */
 void dgemm_MIPP(type* A, type* B, type* C) {
   size_t i, j, k;
 
   for (i = 0; i < N; i++)
-    for (j = 0; j < N; j++)
-      for (k = 0; k < N; k++) C[i * N + j] += A[i * N + k] * B[k * N + j];
+    for (j = 0; j < N; j++) {
+        type tmp = 0;
+
+        for(int n = 0; n < N; n += mipp::nElReg<type>()) {
+          v1.load(&A[i * N + n]);
+          v2.load(&B[i * N + n]);
+
+          auto mul_res = v1 * v2;
+          tmp += mipp::Reduction<type, mipp::add>::sapply(mul_res);
+        }
+        C[i * N + j] = tmp;
+      }
 }
 
 void printM(type* M) {
@@ -40,6 +60,7 @@ void printM(type* M) {
 int main(int argc, char const* argv[]) {
   type* A = (type*)valloc(SIZE * sizeof(double));
   type* B = (type*)valloc(SIZE * sizeof(double));
+  type* Bt = (type*)valloc(SIZE * sizeof(double));
   type* C = (type*)valloc(SIZE * sizeof(double));
   if (A == NULL || B == NULL || C == NULL) exit(EXIT_FAILURE);
 
@@ -50,9 +71,14 @@ int main(int argc, char const* argv[]) {
   }
 
   dgemm(A, B, C);
+  printM(C);
 
-  // printM(A);
-  // printM(B);
+  for (size_t i = 0; i < N; ++i)
+    for (size_t j = 0; j < N; ++j)
+        Bt[j * N + i] = B[i * N + j];
+
+  dgemm_MIPP(A, Bt, C);
+
   // printM(C);
 
   free(A);
