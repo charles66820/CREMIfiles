73987536456a666b2cb4af224f29037a734a7da4
charles66820 <charles.goedefroit@gmail.com>
2022-11-17 20:21:27 +0100

    ça fonctionnnnnnnnnnnnnnne

--- IS323_STD_PLE/TP2/mapreduce_topkeywords/src/main/java/DecadeReducer.java
+++ IS323_STD_PLE/TP2/mapreduce_topkeywords/src/main/java/DecadeReducer.java
@@ -6,8 +6,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DecadeReducer extends Reducer<IntWritable, Text, NullWritable, Text> {
     public void setup(Context context) throws IOException, InterruptedException {
@@ -15,34 +14,36 @@ public class DecadeReducer extends Reducer<IntWritable, Text, NullWritable, Text
     }
 
     public void reduce(IntWritable decade, Iterable<Text> keywords, Context context) throws IOException, InterruptedException {
+        List<String> saveKeywords = new ArrayList<>();
         // Count number of paper for each keyword
         Map<String, Integer> keywordsCount = new HashMap<>();
-//        boolean isNew = false;
+        boolean isNew = false;
         for (Text value : keywords) {
             String keyword = value.toString();
+            saveKeywords.add(keyword);
             String[] keywordsTab = keyword.split(";");
-//            if (keywordsTab.length == 1) isNew = true;
+            System.out.println(Arrays.toString(keywordsTab));
+            if (keywordsTab.length == 1) isNew = true;
             keywordsCount.merge(keywordsTab[0], 1, Integer::sum);
         }
 
-//        if (!isNew) {
-//            for (Text keyword : keywords)
-//                context.write(NullWritable.get(), new Text(decade + ";" + keyword.toString()));
-//            return;
-//        }
+        if (isNew) {
+            // Top keywords by decades
+            final TreeMultimap<Integer, String> topKeywords = TreeMultimap.create(Ordering.natural().reverse(), Ordering.natural());
+            for (Map.Entry<String, Integer> entry : keywordsCount.entrySet())
+                topKeywords.put(entry.getValue(), entry.getKey());
 
-        // Top keywords by decades
-        final TreeMultimap<Integer, String> topKeywords = TreeMultimap.create(Ordering.natural().reverse(), Ordering.natural());
-        for (Map.Entry<String, Integer> entry : keywordsCount.entrySet())
-            topKeywords.put(entry.getValue(), entry.getKey());
-
-        int decadeTop = 1;
-        for (Map.Entry<Integer, String> entry : topKeywords.entries()) {
-            Integer nbPaperInDecade = entry.getKey();
-            String keyword = entry.getValue();
-            // decade : keyword ; nbPaperInDecade ; decadeTop
-            context.write(NullWritable.get(), new Text(decade + ";" + keyword + ";" + nbPaperInDecade + ";" + decadeTop));
-            decadeTop += 1;
+            int decadeTop = 1;
+            for (Map.Entry<Integer, String> entry : topKeywords.entries()) {
+                Integer nbPaperInDecade = entry.getKey();
+                String keyword = entry.getValue();
+                // decade : keyword ; nbPaperInDecade ; decadeTop
+                context.write(NullWritable.get(), new Text(decade + ";" + keyword + ";" + nbPaperInDecade + ";" + decadeTop));
+                decadeTop += 1;
+            }
+        } else {
+            for (String keyword : saveKeywords)
+                context.write(NullWritable.get(), new Text(decade + ";" + keyword));
         }
     }
 }
