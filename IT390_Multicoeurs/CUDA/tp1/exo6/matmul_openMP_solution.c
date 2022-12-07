c56005145bce748ec3da4a7aa55e39a3b86b6dd6
Charles Goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2022-12-07 12:07:14 +0100

    oui

--- IT390_Multicoeurs/CUDA/tp1/exo6/matmul_openMP_solution.c
+++ IT390_Multicoeurs/CUDA/tp1/exo6/matmul_openMP_solution.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
+
 #define TILE_WIDTH 32
 #define SIZE 20
 
-
-int main(void)
-{
+int main(void) {
   float *A, *B, *C;
   int nbLigneA, nbLigneB, nbColA, nbColB;
 
@@ -16,15 +15,14 @@ int main(void)
   nbColA = TILE_WIDTH * SIZE;
   nbColB = TILE_WIDTH * SIZE;
 
-  A = (float*) malloc(nbLigneA * nbColA * sizeof(float));
-  B = (float*) malloc(nbLigneB * nbColB * sizeof(float));
-  C = (float*) malloc(nbLigneA * nbColB * sizeof(float));
+  A = (float *)malloc(nbLigneA * nbColA * sizeof(float));
+  B = (float *)malloc(nbLigneB * nbColB * sizeof(float));
+  C = (float *)malloc(nbLigneA * nbColB * sizeof(float));
 
   /* Initialisation de x et y*/
   for (int i = 0; i < nbLigneA * nbColA; i++) {
     A[i] = 1.0;
   }
-
 
   for (int i = 0; i < nbLigneB * nbColB; i++) {
     B[i] = 2.0;
@@ -33,23 +31,24 @@ int main(void)
   for (int i = 0; i < nbLigneB * nbColB; i++) {
     C[i] = 0.0;
   }
-  
 
   struct timeval tv;
   struct timeval start_tv;
-  
+
   gettimeofday(&start_tv, NULL);
-  
+
 #pragma omp parallel for
-  for(int i = 0; i < nbLigneA; i++)
-      for(int j = 0; j < nbColB; j++)
-	for(int k = 0; k < nbColA; k++)
-	  C[i * nbColA + j] = C[i * nbColA + j] + A[i * nbColA + k] * B[k + nbLigneB * j];
+  for (int i = 0; i < nbLigneA; i++)
+    for (int j = 0; j < nbColB; j++)
+      for (int k = 0; k < nbColA; k++)
+        C[i * nbColA + j] =
+            C[i * nbColA + j] + A[i * nbColA + k] * B[k + nbLigneB * j];
 
   gettimeofday(&tv, NULL);
   double elapsed = ((tv.tv_sec - start_tv.tv_sec) +
-		    (tv.tv_usec - start_tv.tv_usec) / 1000000.0) * 1000;
-  
+                    (tv.tv_usec - start_tv.tv_usec) / 1000000.0) *
+                   1000;
+
   printf("it took %f ms to execute \n", elapsed);
   free(A);
   free(B);
