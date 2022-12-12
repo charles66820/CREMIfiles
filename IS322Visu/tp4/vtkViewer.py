0eb820de396d977a4e3fa0a69eefda2b2def6b4f
charles66820 <charles.goedefroit@gmail.com>
2022-12-12 12:52:33 +0100

    continue

--- IS322Visu/tp4/vtkViewer.py
+++ IS322Visu/tp4/vtkViewer.py
@@ -2,7 +2,7 @@ import vtk
 
 dataPath = "data/"
 
-def createWindow(renderer):
+def createWindow(renderer, lut):
   window = vtk.vtkRenderWindow()
   window.SetSize(500, 500)
   window.SetWindowName("VTK Viewer")
@@ -12,26 +12,37 @@ def createWindow(renderer):
   interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
   interactor.SetRenderWindow(window)
 
+  scalarBar = vtk.vtkScalarBarActor()
+  scalarBar.SetOrientationToHorizontal()
+  scalarBar.SetLookupTable(lut)
+
+  scalarBarWidget = vtk.vtkScalarBarWidget()
+  scalarBarWidget.SetInteractor(interactor)
+  scalarBarWidget.SetScalarBarActor(scalarBar)
+  scalarBarWidget.On()
+
   window.Render()
   interactor.Start()
 
-def coneMapper():
+def getConeMapper():
   cone = vtk.vtkConeSource()
 
   mapper = vtk.vtkPolyDataMapper()
   mapper.SetInputConnection(cone.GetOutputPort())
   return mapper
 
-def headMapper():
+def getHeadMapper():
   reader = vtk.vtkXMLImageDataReader()
   reader.SetFileName(dataPath + "head.vti")
   reader.Update()
 
   readerData = reader.GetOutput()
-  headComponent = readerData.GetPointData().GetScalars("head")
+  headScalarComponent = readerData.GetPointData().GetScalars("head")
 
   print("Head scalars value range :")
-  print(headComponent.GetRange())
+  print(headScalarComponent.GetRange())
+  headScalarRange = readerData.GetScalarRange()
+  print(headScalarRange)
 
   # filters
   vtk.vtkContourFilter()
@@ -44,27 +55,40 @@ def headMapper():
   # mapper
   mapper = vtk.vtkDataSetMapper()
   # mapper.ScalarVisibilityOff()
+  mapper.SetScalarRange(headScalarRange)
   mapper.SetInputConnection(contour.GetOutputPort())
   return mapper
 
 def main():
-  actor = vtk.vtkActor()
-#   actor.SetMapper(coneMapper())
-  actor.SetMapper(headMapper())
-  # show the edges of the image grid
-#   actor.GetProperty().SetRepresentationToWireframe()
+  # cone
+  coneActor = vtk.vtkActor()
+  coneActor.SetMapper(getConeMapper())
 
+  # head
+  headMapper = getHeadMapper()
+
+  lut = vtk.vtkLookupTable()
+  lut.Build()
+  headMapper.SetLookupTable(lut)
+
+  headActor = vtk.vtkActor()
+  headActor.SetMapper(headMapper)
+  # show the edges of the image grid
+  # headActor.GetProperty().SetRepresentationToWireframe()
+
+  # render
   renderer = vtk.vtkRenderer()
   renderer.SetBackground(0.0, 0.0, 0.0)
-  renderer.AddActor(actor)
+  renderer.AddActor(headActor)
+  # renderer.AddActor(coneActor)
 
   camera = renderer.GetActiveCamera()
-  camera.SetRoll(180);
-  camera.Elevation(90);
-  camera.Azimuth(0);
-  renderer.ResetCamera();
+  camera.SetRoll(180)
+  camera.Elevation(90)
+  camera.Azimuth(0)
+  renderer.ResetCamera()
 
-  createWindow(renderer)
+  createWindow(renderer, lut)
 
 if __name__ == "__main__":
   main()
