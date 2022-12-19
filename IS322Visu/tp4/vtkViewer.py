fda57b0a255fcadf3a7673a4f6035fe3d11b1982
charles66820 <charles.goedefroit@gmail.com>
2022-12-19 16:20:32 +0100

    continue

--- IS322Visu/tp4/vtkViewer.py
+++ IS322Visu/tp4/vtkViewer.py
@@ -2,15 +2,39 @@ import vtk
 
 dataPath = "data/"
 
-contour = vtk.vtkContourFilter()
+def getConeMapper():
+  cone = vtk.vtkConeSource()
 
-class SliderObserver(object):
-  def __init__(self, slider, contour):
-    self.slider = slider
-    self.contour = contour
+  mapper = vtk.vtkPolyDataMapper()
+  mapper.SetInputConnection(cone.GetOutputPort())
+  return mapper
 
-  def __call__(self, caller, ev):
-    self.contour.SetValue(0, self.slider.GetValue())
+def getHeadMapper():
+  reader = vtk.vtkXMLImageDataReader()
+  reader.SetFileName(dataPath + "head.vti")
+  reader.Update()
+
+  readerData = reader.GetOutput()
+  # headScalarComponent = readerData.GetPointData().GetScalars("head")
+  # print(headScalarComponent.GetRange())
+
+  print("Head scalars value range :")
+  headScalarRange = readerData.GetScalarRange()
+  print(headScalarRange)
+
+  # filters
+
+  ## iso-surface
+  contour = vtk.vtkContourFilter()
+  contour.SetInputData(reader.GetOutput())
+  contour.SetValue(0, 20)
+
+  # mapper
+  mapper = vtk.vtkDataSetMapper()
+  # mapper.ScalarVisibilityOff()
+  mapper.SetScalarRange(headScalarRange)
+  mapper.SetInputConnection(contour.GetOutputPort())
+  return (mapper, contour)
 
 def addScalarBarWidget(interactor, objMapper):
   lut = vtk.vtkLookupTable()
@@ -24,11 +48,18 @@ def addScalarBarWidget(interactor, objMapper):
   scalarBarWidget = vtk.vtkScalarBarWidget()
   scalarBarWidget.SetInteractor(interactor)
   scalarBarWidget.SetScalarBarActor(scalarBar)
-  scalarBarWidget.On()
+  scalarBarWidget.EnabledOn()
   return scalarBarWidget
 
+class SliderObserver(object):
+  def __init__(self, slider, contour):
+    self.slider = slider
+    self.contour = contour
 
-def addSliderWidget(interactor, objMapper):
+  def __call__(self, caller, ev):
+    self.contour.SetValue(0, self.slider.GetValue())
+
+def addSliderWidget(interactor, objMapper, contour):
   slider = vtk.vtkSliderRepresentation2D()
 
   objScalarRange = objMapper.GetScalarRange()
@@ -57,15 +88,38 @@ def addSliderWidget(interactor, objMapper):
   sliderWidget.AddObserver("InteractionEvent", SliderObserver(slider, contour))
   return sliderWidget
 
-def createWindow(renderer):
-  # addConeActor(renderer)
-  headActor = addHeadActor(renderer)
+def addConeActor(renderer, interactor):
+  coneActor = vtk.vtkActor()
+  coneActor.SetMapper(getConeMapper())
+  renderer.AddActor(coneActor)
+  return coneActor
+
+def addHeadActor(renderer, interactor):
+  headActor = vtk.vtkActor()
+  (headMapper, contour) = getHeadMapper()
+  headActor.SetMapper(headMapper)
+  # show the edges of the image grid
+  # headActor.GetProperty().SetRepresentationToWireframe()
+  renderer.AddActor(headActor)
+
+  # headMapper = headActor.GetMapper()
+  scalarBarWidget = addScalarBarWidget(interactor, headMapper)
+  sliderWidget = addSliderWidget(interactor, headMapper, contour)
+
+  return (scalarBarWidget, sliderWidget)
+
+def createRenderer():
+  renderer = vtk.vtkRenderer()
+  renderer.SetBackground(0.0, 0.0, 0.0)
 
   camera = renderer.GetActiveCamera()
   camera.SetRoll(180)
   camera.Elevation(90)
   camera.Azimuth(0)
-  renderer.ResetCamera()
+  return renderer
+
+def createWindow():
+  renderer = createRenderer()
 
   window = vtk.vtkRenderWindow()
   window.SetSize(500, 500)
@@ -76,68 +130,17 @@ def createWindow(renderer):
   interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
   interactor.SetRenderWindow(window)
 
-  headMapper = headActor.GetMapper()
-  scalarBarWidget = addScalarBarWidget(interactor, headMapper)
-  sliderWidget = addSliderWidget(interactor, headMapper)
+  # addConeActor(renderer, interactor)
+  _ = addHeadActor(renderer, interactor)
+
+  # Always after add actors to render
+  renderer.ResetCamera()
 
   window.Render()
   interactor.Start()
 
-def getConeMapper():
-  cone = vtk.vtkConeSource()
-
-  mapper = vtk.vtkPolyDataMapper()
-  mapper.SetInputConnection(cone.GetOutputPort())
-  return mapper
-
-def getHeadMapper():
-  reader = vtk.vtkXMLImageDataReader()
-  reader.SetFileName(dataPath + "head.vti")
-  reader.Update()
-
-  readerData = reader.GetOutput()
-  headScalarComponent = readerData.GetPointData().GetScalars("head")
-
-  print("Head scalars value range :")
-  print(headScalarComponent.GetRange())
-  headScalarRange = readerData.GetScalarRange()
-  print(headScalarRange)
-
-  # filters
-  vtk.vtkContourFilter()
-
-  ## iso-surface
-  # contour = vtk.vtkContourFilter()
-  contour.SetInputData(reader.GetOutput())
-  contour.SetValue(0, 20)
-
-  # mapper
-  mapper = vtk.vtkDataSetMapper()
-  # mapper.ScalarVisibilityOff()
-  mapper.SetScalarRange(headScalarRange)
-  mapper.SetInputConnection(contour.GetOutputPort())
-  return mapper
-
-def addConeActor(renderer):
-  coneActor = vtk.vtkActor()
-  coneActor.SetMapper(getConeMapper())
-  renderer.AddActor(coneActor)
-  return coneActor
-
-def addHeadActor(renderer):
-  headActor = vtk.vtkActor()
-  headActor.SetMapper(getHeadMapper())
-  # show the edges of the image grid
-  # headActor.GetProperty().SetRepresentationToWireframe()
-  renderer.AddActor(headActor)
-  return headActor
-
 def main():
-  # render
-  renderer = vtk.vtkRenderer()
-  renderer.SetBackground(0.0, 0.0, 0.0)
-
-  createWindow(renderer)
+  createWindow()
 
 if __name__ == "__main__":
   main()
