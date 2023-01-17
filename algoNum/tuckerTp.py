0061d31e4aaab20c29859bf542f53f74b369b8d0
Charles Goedefroit <charles.goedefroit@enseirb-matmeca.fr>
2023-01-17 12:32:49 +0100

    HOSVD tronquée good ?

--- algoNum/tuckerTp.py
+++ algoNum/tuckerTp.py
@@ -25,32 +25,47 @@ def loadTensor1():
     r = multilinéaire
 """
 def truncatedHOSVD(X, r):
-    Us = []
     d = r.size
+    U_ = [None] * d
     for k in range(d):
         # la k-matricisation du tenseur X
         Xᵏ = tl.unfold(X, k)
         
         # SVD tronquée à l'ordre rₖ
-        U, Σ, V = np.linalg.svd(Xᵏ, full_matrices=False)
-        print(U)
-        print(Σ)
-        print(V)
-        # Us[k] =  
-        
-        
+        # U, Σ, V = np.linalg.svd(Xᵏ, full_matrices=False)
+        # U_[k] = U[:,0:r[k]]
+        U, Σ, V = tl.partial_svd(Xᵏ)
+        U_[k] = U
+
+    S = tla.multi_mode_dot(X, U_, transpose=True)
+
+    return S, U_
+
+def checkAndPrintDiff(X, Xref):
+    norm = tl.norm(Xref-X)/tl.norm(Xref)
+    print("norm for X and Xref:", norm)
 
 def main():
-    # n = ??? # tensors mode (numpy array of size d)
-    
+    # ordre du tenseur
+    d = 3
+    # dimension du tenseur. tensors mode (numpy array of size d ??)
+    n = np.asarray([8,8,8])
+    # rang du tenseur. multilinear rank (numpy array of size d)
+    r= np.asarray([4,3,2])
+    # r = np.array((12, 40, 40))
+    # Construction d'un tenseur, X, aléatoire d'ordre d et de rang r
+    X = build_tensor(n, r)
+
     # X = loadTensor("data/minst.h5")
-    X = loadTensor1()
+    # X = loadTensor1()
+
     print("Tensor shape:", X.shape)
-    
-    # multilinear rank (numpy array of size d)
-    r = np.array((12, 40, 40))
     print("Tensor multilinear rank:", r)
-    truncatedHOSVD(X, r)
+    
+    S, U = truncatedHOSVD(X, r)    
+    Xref = tl.tucker_to_tensor((S, U))
+    checkAndPrintDiff(X, Xref)
+
 
 if __name__ == "__main__" :
     main()
